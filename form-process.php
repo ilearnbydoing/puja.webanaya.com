a800b400182aea23c16b49a48fdc25374cca8349
Durgesh Gupta <imdurgeshgupta@gmail.com>
2021-07-14 22:26:36 +0530

    Update form-process.php

--- form-process.php
+++ form-process.php
@@ -4,8 +4,8 @@ $email = $_POST["email"];
 $phone = $_POST["phone"];
 $company = $_POST["company"];
 
-$EmailTo = "instrument@vsnl.net,info@hnhtwenty.com,durgesh@webanaya.com";
-$EmailFrom = "info@hnhtwenty.com";
+$EmailTo = "test@test.com";
+$EmailFrom = "test@test.com";
 $Subject = "Puja Engineering - You have received New Enquiry";
 
 // prepare email body text
